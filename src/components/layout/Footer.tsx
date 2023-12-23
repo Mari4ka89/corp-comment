c1852e6de0ea4898239830807f6c01b576cbaf99
Mariia Bezrukava <mari4ka89@gmail.com>
2023-12-23 12:02:59 +0200

    Update  footer with GitHub link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,4 +1,3 @@
-// TODO: specify link to GitHub, update text.
 export default function Footer() {
   return (
     <footer className="footer">
@@ -6,7 +5,7 @@ export default function Footer() {
         <p>&copy; Copyright by Mariia Bezrukava.</p>
         <p>
           This project is open-source on{" "}
-          <a target="_blank" href="">
+          <a target="_blank" href="https://github.com/Mari4ka89/corp-comment">
             GitHub.
           </a>
         </p>
